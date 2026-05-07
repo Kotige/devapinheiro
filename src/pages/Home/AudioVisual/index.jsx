3878158fbb2329baed96a3bc83d2621a42e2eea8
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-05-07 10:55:54 -0300

    fix:adjust imgs

--- src/pages/Home/AudioVisual/index.jsx
+++ src/pages/Home/AudioVisual/index.jsx
@@ -12,8 +12,8 @@ import pecado from "@/assets/images/audiovisual/a5.png"
 import luciano from "@/assets/images/audiovisual/a6.png"
 
 // Carousel
-import still1 from "../../../assets/images/banners/stil1.png"
-import still2 from "../../../assets/images/banners/still2.png"
+import still1 from "@/assets/images/banners/stil1.png"
+import still2 from "@/assets/images/banners/still2.png"
 
 export default function AudioVisual() {
     return (
